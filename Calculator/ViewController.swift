cb1daae65ad39b4861d6c841c9b3cfdb9ca3bcf5
Andriy Krupych <akrupych@ite390.lv.remit.se>
2017-04-18 12:01:14 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Andriy Krupych on 4/18/17.
+//  Copyright © 2017 Andriy Krupych. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
